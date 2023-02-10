38c3571824b069a5189d5747aa425def513ffd82
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-10 09:10:15 +0530

    module weather

--- src/contollers/userController.js
+++ src/contollers/userController.js
@@ -1,28 +1,41 @@
+const geocode = require('../utils/geocode');
+const forecast = require('../utils/forecast');
 
 module.exports = {
-    index(req,res){
-        res.render('index',{
+
+    index(req, res) {
+        res.render('index', {
             title: 'Weather',
             name: 'Riyas'
         })
     },
-    about(req,res){
-        res.render('about',{
+
+    weather(req, res) {
+        if (!req.query.address) {
+            return res.send({
+                error: 'Please provide an address'
+            });
+        }
+
+    },
+
+    about(req, res) {
+        res.render('about', {
             title: 'About',
             name: 'Riyas'
         })
     },
-    help(req,res){
-        res.render('help',{
+    help(req, res) {
+        res.render('help', {
             title: 'Help',
-            helpText:'this is help text',
+            helpText: 'this is help text',
             name: 'Riyas'
         })
     },
-    error(req,res){
-        res.render('404',{
+    error(req, res) {
+        res.render('404', {
             title: 'Error',
-            errorMessage:'this is error text',
+            errorMessage: 'this is error text',
             name: 'Riyas'
         })
     }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../contollers/userController');
 
-
-
 router
     .get('/',userController.index)
+    .get('weather',userController.weather)
     .get('/about',userController.about)
     .get('/help',userController.help)
     .get('*',userController.error)
